5921ba923f2fc606cae7deacce2343d3f0cdf1e2
Marialaina <pmarialaina@gmail.com>
2020-10-16 15:44:50 -0600

    Add files via upload
    I capitalized each element in the array with a message

---
+++ loops.js
@@ -0,0 +1,21 @@
+
+
+const pets = ['gigi-boy','charlie','luna','ruby','cali','moosa-misa'];
+
+// for (let i = 0 ; i <  pets.length; i++ ){
+//     pet = pets[i]
+//     console.log(pet.charAt(0).toUpperCase() + pet.slice(1) + ', is part of the family!');
+// }
+
+//OR CAN BE DONE
+
+for (let i = 0 ; i < pets.length; i++ ) {
+    console.log(pets[i].charAt(0).toUpperCase() + pets[i].slice(1) + ', is part of the family!');
+
+}
+
+
+
+
+
+
